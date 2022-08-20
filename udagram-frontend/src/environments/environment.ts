2277c70fe2316bcb47cdca92bbf9c888d9d9737b
ha.dothithu <ha.dothithu@petronas.com>
2022-08-20 11:27:53 +0700

    update deploy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: "http://Udagram-env.eba-sdudbm3d.us-east-1.elasticbeanstalk.com/"
+  apiHost: "http://Udagram-env.eba-sdudbm3d.us-east-1.elasticbeanstalk.com"
 };
 
 /*
